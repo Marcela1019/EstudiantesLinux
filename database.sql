9b0d0483bb245c7a62046b7255cffc231b159385
Marcela1019 <marcelabetancourt1019@gmail.com>
2023-05-25 08:02:15 -0500

    creo base de datos

--- database.sql
+++ database.sql
@@ -4,5 +4,10 @@ CREATE TABLE campers(
     id INT primary key AUTO_INCREMENT,
     nombres VARCHAR (50) NOT NULL,
     direccion VARCHAR (50),
-    logros VARCHAR (60)
+    logros VARCHAR (60),
+    ser VARCHAR (50),
+    skills VARCHAR (50),
+    ingles VARCHAR (50),
+    review  VARCHAR (50),
+    especialidad VARCHAR (50)
 );
